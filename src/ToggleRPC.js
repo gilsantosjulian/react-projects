5f472d5c126677ee898fdca58384ff3fc16b23b5
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2021-02-04 12:00:38 -0500

    feat: render props with children most eficient way to render props

---
+++ src/ToggleRPC.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const ToggleRPC = ({ children }) => {
+
+  const [on, setOn] = useState(false)
+  
+  const toggle = () => {
+    setOn(!on)
+  }
+
+  return (
+    children({
+      on: on,
+      toggle: toggle,
+    })
+  )
+}
+
+export default ToggleRPC
